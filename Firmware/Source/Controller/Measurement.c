e28bae278327d8ee4585c64b43792fb2eae69d14
asytyi <alexander.a.sytyi@gmail.com>
2023-04-10 14:52:52 +0300

    Переменные в DT изначально флоатовские, поэтому преобразовывать их не нужно

--- Firmware/Source/Controller/Measurement.c
+++ Firmware/Source/Controller/Measurement.c
@@ -6,17 +6,16 @@
 
 float MEASURE_GetPressureValue()
 {
-	float Offset, K, Pressure, P2, P1;
-	int16_t P0;
+	float Offset, K, Pressure, P2, P1, P0;
 
-	Offset = (float)((int16_t)DataTable[REG_PRESSURE_OFFSET]);
-	K = (float)DataTable[REG_PRESSURE_K] / 1000;
+	Offset = DataTable[REG_PRESSURE_OFFSET];
+	K = DataTable[REG_PRESSURE_K];
 	Pressure = ((float)ADC_Measure(ADC1, ADC_PRESSURE_CHANNEL) - Offset) * ADC_REF_VOLTAGE / ADC_RESOLUTION * K;
 
 	// Тонкая подстройка измерения напряжения
-	P2 = (float)((int16_t) DataTable[REG_PRESSURE_P2]) / 1000000;
-	P1 = (float)DataTable[REG_PRESSURE_P1] / 1000;
-	P0 = (int16_t) DataTable[REG_PRESSURE_P0];
+	P2 = DataTable[REG_PRESSURE_P2];
+	P1 = DataTable[REG_PRESSURE_P1];
+	P0 = DataTable[REG_PRESSURE_P0];
 
 	Pressure = Pressure * Pressure * P2 + Pressure * P1 + P0;
 
